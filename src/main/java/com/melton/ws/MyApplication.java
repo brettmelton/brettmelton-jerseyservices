d9a59c89c313134f5fd81036aa4637071e11d15a
brettmelton <brettmelton@gmail.com>
2016-05-02 18:07:32 -0700

    Changed path

--- src/main/java/com/melton/ws/MyApplication.java
+++ src/main/java/com/melton/ws/MyApplication.java
@@ -1,5 +1,7 @@
 package com.melton.ws;
 
+import io.swagger.jaxrs.config.BeanConfig;
+
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -12,7 +14,7 @@ import javax.ws.rs.ext.ContextResolver;
 import org.glassfish.jersey.moxy.json.MoxyJsonConfig;
 import org.glassfish.jersey.server.ResourceConfig;
 
-@ApplicationPath("/services")
+@ApplicationPath("/")
 public class MyApplication extends Application {
 
     public static ResourceConfig createApp() {
@@ -29,17 +31,30 @@ public class MyApplication extends Application {
         return moxyJsonConfig.resolver();
     }
 	
-	
+    public MyApplication() {
+        BeanConfig beanConfig = new BeanConfig();
+        beanConfig.setVersion("1");
+        beanConfig.setSchemes(new String[]{"http"});
+        beanConfig.setHost("services.brettmelton.com:80");
+        beanConfig.setBasePath("/");
+        beanConfig.setResourcePackage("com.melton.ws");
+        beanConfig.setScan(true);
+    }
+    
 	@Override
     public Set<Class<?>> getClasses() {
         Set<Class<?>> resources = new HashSet<Class<?>>();
-        resources.add(MyResource.class);
+        resources.add(com.melton.ws.MyResource.class);
 
         //Manually adding MOXyJSONFeature
         resources.add(org.glassfish.jersey.moxy.json.MoxyJsonFeature.class);
  
         //Configure Moxy behavior
         resources.add(JsonMoxyConfigurationContextResolver.class);
+        
+        // Add in Swagger for Documentation
+        resources.add(io.swagger.jaxrs.listing.ApiListingResource.class);
+        resources.add(io.swagger.jaxrs.listing.SwaggerSerializers.class);
         
         return resources;
     }
